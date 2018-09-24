9e97b0d28419854c54621d89ebda9fc8ddc1bd0a
Marcin <marcinsa12@gmail.com>
2018-09-25 01:38:42 +0200

    frame added, working operators, bug fixed, responsivness within the screen

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,28 @@
   <div id="app">
     <div class="headLine">
       <img src="./assets/logo.png">
-      <HelloWorld/>
+      <headLabel/>
       <img src="./assets/logo.png">
     </div>
-    <calcFrame/>
-
+    <div class="iphoneBody">
+      <div class="speaker">
+      </div>
+      <calcFrame/>
+      <div class="home">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
+import headLabel from "./components/headLabel";
 import calcFrame from "./components/calcFrame";
 import calcButton from "./components/calcButton";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    headLabel,
     calcFrame,
     calcButton
   }
@@ -29,6 +34,59 @@ export default {
 img {
   width: 50px;
   height: 50px;
+}
+
+body {
+  font-size: 28px;
+  font-family: "Lato";
+  font-weight: 300;
+}
+
+.iphoneBody {
+  margin: 0 auto;
+  border: 3px solid	#c9ba93;
+  border-radius: 75px;
+  width: 450px;
+  height: 930px;
+}
+
+.speaker {
+  margin: 56px auto;
+  border-radius: 15px;
+  width: 80px;
+  height: 9px;
+  background: black;
+  position: relative;
+}
+
+.speaker::after{
+  content: '';
+  width: 20px;
+  height: 20px;
+  background: black;
+  position: absolute;
+  left: -50px;
+  top: -5px;
+  border-radius: 50%;
+}
+.speaker::before{
+  content: '';
+  width: 12px;
+  height: 12px;
+  background: black;
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+  top: -45px;
+  border-radius: 50%;
+}
+
+.home {
+  margin: 7px auto;
+  width: 78px;
+  height: 78px;
+  border: 5px solid #c9ba93;
+  border-radius: 50%;
 }
 
 .headLine {
@@ -42,10 +100,5 @@ img {
   text-align: center;
   color: #2c3e50;
   margin-top: 10px;
-}
-body {
-  font-size: 28px;
-  font-family: "Lato";
-  font-weight: 300;
 }
 </style>
--- src/components/calcFrame.vue
+++ src/components/calcFrame.vue
@@ -2,24 +2,24 @@
   <div class="calc">
     <div class="screen"> {{currentState || '0'}}</div>
     <calcButton @click.native="clear" class="specialChar1" v-bind:button="'AC'"/>
-    <calcButton class="specialChar1" v-bind:button="'+/-'"/>
-    <calcButton class="specialChar1" v-bind:button="'%'"/>
-    <calcButton class="specialChar2" v-bind:button="'÷'"/>
+    <calcButton @click.native="positiveNegative" class="specialChar1" v-bind:button="'+/-'"/>
+    <calcButton @click.native="currentState /= 100"class="specialChar1" v-bind:button="'%'"/>
+    <calcButton @click.native="setOperator('/')" class="specialChar2" v-bind:button="'÷'"/>
     <calcButton @click.native="addSymbol('7')" v-bind:button="'7'"/>
     <calcButton @click.native="addSymbol('8')" v-bind:button="'8'"/>
     <calcButton @click.native="addSymbol('9')" v-bind:button="'9'"/>
-    <calcButton class="specialChar2" v-bind:button="'×'"/>
+    <calcButton @click.native="setOperator('*')" class="specialChar2" v-bind:button="'×'"/>
     <calcButton @click.native="addSymbol('4')" v-bind:button="'4'"/>
     <calcButton @click.native="addSymbol('5')" v-bind:button="'5'"/>
     <calcButton @click.native="addSymbol('6')" v-bind:button="'6'"/>
-    <calcButton class="specialChar2" v-bind:button="'-'"/>
+    <calcButton @click.native="setOperator('-')" class="specialChar2" v-bind:button="'-'"/>
     <calcButton @click.native="addSymbol('1')" v-bind:button="'1'"/>
     <calcButton @click.native="addSymbol('2')" v-bind:button="'2'"/>
     <calcButton @click.native="addSymbol('3')" v-bind:button="'3'"/>
-    <calcButton class="specialChar2" v-bind:button="'+'"/>
-    <calcButton class="longButton" v-bind:button="'0'"/>
+    <calcButton @click.native="setOperator('+')" class="specialChar2" v-bind:button="'+'"/>
+    <calcButton @click.native="addSymbol('0')"class="longButton" v-bind:button="'0'"/>
     <calcButton @click.native="addDot" v-bind:button="'.'"/>
-    <calcButton class="specialChar2" v-bind:button="'='"/>
+    <calcButton @click.native="equal" class="specialChar2" v-bind:button="'='"/>
 
   </div>
 </template>
@@ -36,21 +36,56 @@ export default {
     return {
       currentState: "",
       currentOperator: null,
-      oldState: null
+      oldState: null,
+      operators: {
+        "+" : (a,b)=>{return a+b},
+        "-" : (a,b)=>{return a-b},
+        "*" : (a,b)=>{return a*b},
+        "/" : (a,b)=>{return a/b},
+      }
     };
   },
   methods: {
+    toNumber: function(){
+      this.oldState = parseFloat(this.oldState);
+      this.currentState = parseFloat(this.currentState)
+    },
     addSymbol: function(symbol) {
-      this.currentState += symbol;
+      if(symbol == 0) {
+        this.currentState ? this.currentState += symbol : this.currentState;
+      } else {
+        this.currentState += symbol;
+      }
     },
     clear: function() {
       this.currentState = "";
     },
     addDot: function() {
+      this.currentState == '' ? this.currentState = '0' : this.currentState;
       this.currentState.indexOf(".") === -1 ? this.addSymbol(".") : this.currentState;
+
+    },
+    positiveNegative: function() {
+      this.currentState.indexOf("-") === -1 && this.currentState !== '' ? this.currentState = '-'.concat(this.currentState) : this.currentState = this.currentState.substr(1);
+    },
+    setOperator: function(operator) {
+      this.currentOperator = operator;
+      this.oldState = this.currentState;
+      this.clear();
+      console.log(this.currentState)
+    },
+    equal: function(){
+      this.toNumber();
+      this.currentState = parseFloat(this.operators[this.currentOperator](this.oldState, this.currentState).toFixed(6));
     }
   },
-  computed: {}
+  computed: {},
+  watch: {
+    currentState: function(){
+      this.currentState.length >= 7 ? document.querySelector('.screen').style.fontSize = '2.5em' : document.querySelector('.screen').style.fontSize = '3.5em'; // if many chars, change font-size
+      this.currentState.length > 9 ? this.currentState = this.currentState.substring(0,9) : this.currentState; // keeping length of 9 chars MAX
+    }
+  }
 };
 </script>
 
@@ -60,7 +95,7 @@ h1 {
   font-size: 1.2em;
 }
 .calc {
-  border-radius: 5%;
+  border: 3px solid black;
   overflow: hidden;
   background: black;
   display: grid;
@@ -77,6 +112,7 @@ h1 {
   justify-content: flex-end;
   padding: 0 15px;
   font-size: 3.5em;
+
   color: white;
   grid-column-start: 1;
   grid-column-end: 5;
